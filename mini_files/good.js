d6bf8be396350af54103ce72a7fd8d642a702d35
beleiver1979 <christopher.naranjo@wayland.wbu.edu>
2017-05-31 21:22:49 -0500

    initial work on name greet button.

---
+++ mini_files/good.js
@@ -0,0 +1,9 @@
+/**Created by Chris Naranjo on 5/31/2017.*/
+/** Function that gets the users name and greets them*/
+
+function get_greet() {
+    var x = document.getElementById("name").value;
+    alert("Hi,  " + x);
+
+
+}
